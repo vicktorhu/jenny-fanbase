b7e4e57022b42d7334947f891d60f6a3f5389099
Vicktor Hutama <vicktor.hutama@gmail.com>
2021-09-03 19:37:53 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: null
+    };
+  },
+  mounted() {
+    const RSS_URL = `https://cors-anywhere.herokuapp.com/koreatimes.co.kr/www/rss/entertainment.xml`;
+
+    var parseString = require("xml2js").parseString;
+
+    this.$axios.get(RSS_URL).then(response => {
+      var self = this;
+      parseString(response.data, function(err, result) {
+        self.data = result;
+        console.log(result);
+      });
+    });
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
